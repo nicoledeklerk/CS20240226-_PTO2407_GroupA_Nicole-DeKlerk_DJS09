360a441ab7aa4fc122e7b3b5f85c55fe40e3fbab
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-06 13:30:05 +0200

    Create Property Objects Array

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@
 // Write a function that will only accept numbers and attend to
 // all TypeScript weakness flags.
 // : number
-const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
-const reviewTotalDisplay = document.querySelector('#reviews')
+import { showReviewTotal, populateUser } from './utils'
 let isOpen : boolean
 
 const reviews : {
@@ -33,14 +31,6 @@ const reviews : {
   },
 ];
 
-function showReviewTotal (value : number, reviewer : string, isLoyalty : boolean) {
-  const iconDisplay = isLoyalty ? '⭐' : ''
-  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + 
-  reviewer + iconDisplay
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
 const you: {
   firstName : string;
   lastName: string;
@@ -55,12 +45,60 @@ const you: {
  stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-
-function populateUser(isReturning : boolean, userName : string ) {
-  if (isReturning){
-      returningUserDisplay.innerHTML = 'back'
-  }
-  userNameDisplay.innerHTML = userName
+const properties : {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+      firstLine: string;
+      city: string;
+      code: number;
+      country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [
+  {
+    image: '',
+    title: 'Colombian Shack',
+    price: 45,
+    location: {
+        firstLine: 'shack 37',
+        city: 'Bogota',
+        code: 45632,
+        country: 'Colombia'
+    },
+    contact: 'marywinkle@gmail.com',
+    isAvailable: true  
+},
+{
+    image: '',
+    title: 'Polish Cottage',
+    price: 34,
+    location: {
+        firstLine: 'no 23',
+        city: 'Gdansk',
+        code: 343903,
+        country: 'Poland'
+    },
+    contact: 'garydavis@hotmail.com',
+    isAvailable: false 
+},
+{
+    image: '',
+    title: 'London Flat',
+    price: 23,
+    location: {
+        firstLine: 'flat 15',
+        city: 'London',
+        code: 35433,
+        country: 'United Kingdom',
+    },
+    contact: 'andyluger@aol.com',
+    isAvailable: true
 }
+]
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
