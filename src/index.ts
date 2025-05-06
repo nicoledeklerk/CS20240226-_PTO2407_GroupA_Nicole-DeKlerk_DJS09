a3f4f5665b0ac287eddf43afcb2445106d62ad66
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-06 13:13:56 +0200

    Assign Type To const reviews

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,12 @@ const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 let isOpen : boolean
 
-const reviews = [
+const reviews : {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [ 
   {
     name: "Sheia",
     stars: 5,
