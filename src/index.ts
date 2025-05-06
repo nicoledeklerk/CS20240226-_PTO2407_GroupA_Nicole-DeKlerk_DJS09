69157aaefc77e657bb1ef1188acc552a0085c657
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-06 12:46:25 +0200

    Type Inference Feature

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
+let isOpen : boolean
+
 const reviews = [
   {
     name: "Sheia",
