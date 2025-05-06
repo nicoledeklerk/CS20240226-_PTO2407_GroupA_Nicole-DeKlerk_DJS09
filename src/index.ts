bb08bbfbe3cde35976c8ce998b7ac17bf741a3fa
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-06 15:23:38 +0200

    Use Enum Types

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,39 @@
+import { showReviewTotal, populateUser } from './utils'
+import { Permissions, LoyaltyUser } from './enums'
 const propertyContainer = document.querySelector('.properties')
 const footer = document.querySelector('.footer')
-
-import { showReviewTotal, populateUser } from './utils'
 let isOpen: boolean
 
 const reviews : {
   name: string;
   stars: number;
-  loyaltyUser: boolean;
+  loyaltyUser: LoyaltyUser;
   date: string;
   }[] = [ 
   {
     name: "Sheia",
     stars: 5,
-    loyaltyUser: true,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
     date: "01-04-2021"
   },
   {
     name: "Andrzej",
     stars: 3,
-    loyaltyUser: false,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
     date: "28-03-2021"
   },
   {
     name: "Omar",
     stars: 4,
-    loyaltyUser: true,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
     date: "27-03-2021"
   },
 ];
 
-const you: {
-  firstName : string;
-  lastName: string;
-  isReturning: boolean;
-  age: number;
-  stayedAt: string[];
-} = {
+const you = {
  firstName: 'Bobby',
  lastName: 'Brown',
+ permissions: Permissions.ADMIN,
  isReturning: true,
  age: 35,
  stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
