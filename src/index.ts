7ba00464c2dab8b8d3d9738ebc7f37836badec21
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-06 16:01:27 +0200

    Use Literal Types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { showReviewTotal, populateUser } from './utils'
 import { Permissions, LoyaltyUser } from './enums'
+import { Price, Country } from './types'
 const propertyContainer = document.querySelector('.properties')
 const footer = document.querySelector('.footer')
 
@@ -44,7 +45,7 @@ const properties : {
       firstLine: string;
       city: string;
       code: number;
-      country: string;
+      country: Country;
   };
   contact: [number, string];
   isAvailable: boolean;
